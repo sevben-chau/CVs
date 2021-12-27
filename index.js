2d2b33181ec298ec89c0d3dad317b78031f75b43
sevbenchau <asfsdj123456@gmail.com>
2021-12-27 17:46:59 +0700

    2nd

---
+++ index.js
@@ -0,0 +1,9 @@
+const loading = document.querySelector("loading");
+var video = document.getElementById("bg-video");
+var content = document.getElementById("content");
+console.log("dsadas");
+setTimeout(() => {
+  document.getElementById("loading").style.display = "none";
+  console.log("đã chuyển");
+}, 2500);
+clearTimeout();
