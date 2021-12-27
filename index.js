9f2e54f8e227205f40d3ea48e10f9276d7dc0226
SevbenChau <sevben2356@gmail.com>
2021-12-27 18:47:00 +0700

    dsa4

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const loading = document.querySelector("loading");
-var video = document.getElementById("bg-video");
-var content = document.getElementById("content");
+// document.getElementById("bg-video").style.display = "none";
+// document.getElementById("content").style.display = "none";
 console.log("dsadas");
 setTimeout(() => {
   document.getElementById("loading").style.display = "none";
+  document.getElementById("bg-video").style.display = "block";
+  document.getElementById("content").style.display = "flex";
   console.log("đã chuyển");
 }, 2500);
 clearTimeout();
